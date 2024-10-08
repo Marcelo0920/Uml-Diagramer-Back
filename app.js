49c2782fa8fdaade9af79900a7acd6befaa55e9c
Marcelo0920 <marcelo0920.z@gmail.com>
2024-10-08 15:26:43 -0400

    added new socket url

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://staging.d29zhbrzfs1bz0.amplifyapp.com/",
     methods: ["GET", "POST", "UPDATE", "DELETE"],
   },
 });
